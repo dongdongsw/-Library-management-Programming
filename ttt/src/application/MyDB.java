798734c1aeb3a29adddfcdfceb384639c973cf09
SeoDongHyeon <fishman4535@gmail.com>
2024-03-19 20:40:41 +0900

    메인이 되는 소스 코드

---
+++ ttt/src/application/MyDB.java
@@ -0,0 +1,16 @@
+package application;
+
+import java.util.HashMap;
+
+public class MyDB {
+
+		public HashMap<String, String> table;
+		public MyDB() {
+			table = new HashMap<>();
+			table.put("아이디", "비밀번호");
+			table.put("ID", "PW");
+		}
+		
+	}
+
+
