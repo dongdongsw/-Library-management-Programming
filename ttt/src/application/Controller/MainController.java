c3647186f012dc0c21f0cecd958526cabd7b2098
SeoDongHyeon <fishman4535@gmail.com>
2024-04-09 11:44:10 +0900

    파일 이름 변경 및 디렉토리 위치 이동

--- ttt/src/application/Controller/MainController.java
+++ ttt/src/application/Controller/MainController.java
@@ -1,4 +1,4 @@
-package application;
+package application.Controller;
 
 import java.io.IOException;
 import javafx.event.ActionEvent;
@@ -22,7 +22,7 @@ public class MainController {
 		if(txtUserName.getText().equals("user") && txtPassword.getText().equals("pass")){
 			lblStatus.setText("Login Success");
 			Stage primaryStage = new Stage();
-			Parent root = FXMLLoader.load(getClass().getResource("/application/Book_User_Search.fxml"));
+			Parent root = FXMLLoader.load(getClass().getResource("/application/Main/MainMenu.fxml"));
 			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
--- ttt/src/application/Table/MyDB.java
+++ ttt/src/application/Table/MyDB.java
@@ -1,4 +1,4 @@
-package application;
+package application.Table;
 
 import java.util.HashMap;
 
