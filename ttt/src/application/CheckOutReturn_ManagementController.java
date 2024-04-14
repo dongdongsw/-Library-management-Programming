fdb1d178ba5c8320ddfd1a7e0c38fe5564c96949
gihan96 <gihan12@naver.com>
2024-04-14 19:05:50 +0900

    <수정된 것>

---
+++ ttt/src/application/CheckOutReturn_ManagementController.java
@@ -0,0 +1,43 @@
+package application;
+
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import java.io.IOException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Button;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+
+public class CheckOutReturn_ManagementController {
+	@FXML
+	private Button Home, LogOut, Exit;
+	
+	@FXML
+	private void handleHomeButtonAction(ActionEvent event) {
+		//홈 버튼을 눌렀을 때 대출&반납 메뉴가 꺼짐과 동시에 메인화면으로 전환
+		try {
+	
+			Parent Root = FXMLLoader.load(getClass().getResource("/application/Ui/Main/MainMenu.fxml"));
+            Scene scene = new Scene(Root);
+            scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+
+            
+         // 현재 스테이지 가져오기 (현재 이벤트가 발생한 Window를 기반으로)
+            Stage currentStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            
+            // 새로운 씬 설정하고 보이기
+            currentStage.setScene(scene);
+            currentStage.show();
+        } 
+		catch (IOException e) {
+        	e.printStackTrace();
+        }
+  		
+	}
+	
+
+}
